a2f777a3d4ecb0d681e26182374222db436297a3
wangyanxin <wangyanxin@xiaomi.com>
2019-07-13 06:37:17 +0800

    upd 添加新路由

--- src/config/routeMenu.js
+++ src/config/routeMenu.js
@@ -1,16 +1,34 @@
 import quillEdit from '@/views/quillEdit'
+import restaurant from '@/views/restaurant'
+import home from '@/views'
 
 const routeMap = [
   {
     path: '/',
     name: '首页',
-    redirect: '/quillEdit'
+    component: home,
+    id: 1,
+    meta: {
+      index: '1'
+    }
   },
   {
     path: '/quillEdit',
     name: '富文本编辑器',
+    id: 2,
     component: quillEdit,
-    meta: {}
+    meta: {
+      index: '2'
+    }
+  },
+  {
+    path: '/restaurant',
+    name: 'restaurant',
+    id: 3,
+    component: restaurant,
+    meta: {
+      index: '3'
+    }
   }
 ]
 export default routeMap;
