4a5d332a4a49c75151c71270c91b9a6fbba0e634
wangyanxin <wangyanxin@xiaomi.com>
2019-07-13 06:37:53 +0800

    add 引入element-UI 和全局less

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/route'
+import ElementUI from 'element-ui';
+import '@/assets/css/common.less'
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
