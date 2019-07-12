aa541c716d754e4ad4d25d782bbd470c6fed97f7
wangyanxin <wangyanxin@xiaomi.com>
2019-07-13 06:39:05 +0800

    add 添加富文本编辑器

--- src/views/quillEdit/index.vue
+++ src/views/quillEdit/index.vue
@@ -1,17 +1,63 @@
 <template>
-  <div class="quill-edit-container">{{name}}</div>
+  <div class="quill-edit-container">
+    <div class="mudule-name">vue-quill-edit  demo</div>
+    <quill-editor 
+      v-model="content"
+      ref="myQuillEditor"
+      :options="editorOption"
+      @blur="onEditorBlur($event)"
+      @focus="onEditorFocus($event)"
+      @ready="onEditorReady($event)">
+    </quill-editor>
+  </div>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        name: 'edit'
-      }
+import Quill from 'quill'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
+// 引入图片扩展功能
+import { ImageDrop } from 'quill-image-drop-module';
+// import { ImageResize } from 'quill-image-resize-module/image-resize.min.js';
+
+
+Quill.register('modules/imageDrop', ImageDrop);
+// Quill.register('modules/imageResize', ImageResize);
+
+
+
+export default {
+  components: {
+    quillEditor
+  },
+  data () {
+    return {
+      name: 'edit',
+      editorOption: {
+        modules: {
+          imageDrop: true,
+          // ImageResize: {}
+        }
+      },
+      content: ''
+    }
+  },
+  methods: {
+    onEditorBlur (e) {
+      console.log(e, '>>>>>>onEditorBlur')
+    },
+    onEditorFocus (e) {
+      console.log(e, '>>>>>>onEditorFocus')
+    },
+    onEditorReady (e) {
+      console.log(e, '>>>>>>onEditorReady')
     }
   }
+}
 </script>
 
-<style>
+<style lang='less'>
 .quill-edit-container {
   color: #333;
 }
