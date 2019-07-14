3c0972d866a1cc4cb339124679418c89ed538541
wangyanxin <wangyanxin@xiaomi.com>
2019-07-14 11:19:12 +0800

    add 添加菜馆map

---
+++ src/views/restaurant/map.js
@@ -0,0 +1,42 @@
+export const restautantMap = [
+  {
+    name: '老西面馆',
+    color: ''
+  },
+  {
+    name: '粥铺',
+    color: ''
+  },
+  {
+    name: '吉祥混沌',
+    color: ''
+  },
+  {
+    name: '新疆面馆',
+    color: ''
+  },
+  {
+    name: '熊大',
+    color: ''
+  },
+  {
+    name: '酱骨头',
+    color: ''
+  },
+  {
+    name: '杨国福',
+    color: ''
+  },
+  {
+    name: '黄焖鸡',
+    color: ''
+  },
+  {
+    name: '串串餐厅',
+    color: ''
+  },
+  {
+    name: '湘韶风情',
+    color: ''
+  }
+]
